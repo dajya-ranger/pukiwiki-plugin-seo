392e5647c833dbd0a42b864d67fbdb69ac2ac323
dajya-ranger <red@dajya-ranger.com>
2020-10-18 18:16:08 +0900

    v0.2.0リリースコミット

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 PukiWiki用SEO対応プラグイン
 
-- 暫定公開版です（[PukiWiki1.5.2](https://pukiwiki.osdn.jp/?PukiWiki/Download/1.5.2)で動作確認済）
+- 暫定公開版です（[PukiWiki1.5.2](https://pukiwiki.osdn.jp/?PukiWiki/Download/1.5.2)及び[PukiWiki1.5.3](https://pukiwiki.osdn.jp/?PukiWiki/Download/1.5.3)で動作確認済）
 - 本プラグインを利用することで、[PukiWiki](https://ja.wikipedia.org/wiki/PukiWiki)サイトのページ単位で次のメタタグを設定することが可能です
 	- description
 	- og:description（Facebookへのページ概要OGPタグ）
 	- twitter:description（Twitterへのページ概要OGPタグ）
-	- keywords
+	- article:tag（Ver0.2.0より「keywords」タグから変更）
 - 本プラグインでは、[PukiWiki](https://ja.wikipedia.org/wiki/PukiWiki)の[ページ階層化機能](https://pukiwiki.osdn.jp/?Use+PukiWiki/%E3%83%9A%E3%83%BC%E3%82%B8%E3%81%AE%E9%9A%8E%E5%B1%A4%E5%8C%96)と[カテゴリー機能](https://pukiwiki.osdn.jp/?Use+PukiWiki/%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC)を応用し、ページ単位にタグ（もしくはサイトの運用形態によっては「カテゴリー」としても利用可）を追加することが可能です
 - [SEO](https://ja.wikipedia.org/wiki/%E6%A4%9C%E7%B4%A2%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%B3%E6%9C%80%E9%81%A9%E5%8C%96)に関して、keywordsメタタグによるメタキーワードについては「[メタキーワードとは？ SEO対策として効果がなくなった背景について](https://innova-jp.com/seo-meta-keyword/)」を、[タグクラウド](https://www.itmedia.co.jp/im/articles/0702/28/news127.html)を含むページのタグ付けに関しては「[タグクラウドの作り方とSEO効果教えます](https://seolaboratory.jp/38902/)」等の記事をそれぞれ参照して下さい
 - FacebookおよびTwitterのOGPタグの詳細に関しては自サイトの記事「[PukiWiki1.5.2をソーシャルメディアに接続してFacebookコメントを実装する！](https://dajya-ranger.com/pukiwiki/connect-social-media/)」を参照して下さい
 - 設置に関しては自サイトの記事「[PukiWikiにSEO対応プラグインを導入してサイト構成を変更する！](https://dajya-ranger.com/pukiwiki/seo-support-plugin/)」を参照して下さい
+- Ver0.1.1からの変更点は次の通り
+	- ページキーワードの出力タグ変更（keywords→article:tag）
+	- タグページを毎回出力（上書き作成→既存のタグページを書き換え）
+	- タグページに本プラグインを利用したdescriptionとarticle:tagタグを出力
--- seo.inc.php
+++ seo.inc.php
@@ -6,13 +6,14 @@
  * SEO対応プラグイン
  *
  * @author		オヤジ戦隊ダジャレンジャー <red@dajya-ranger.com>
- * @copyright	Copyright © 2019, dajya-ranger.com
+ * @copyright	Copyright © 2019-2020, dajya-ranger.com
  * @link		https://dajya-ranger.com/pukiwiki/seo-support-plugin/
  * @example		&seo(description){description};
  * @example		&seo(keywords){keyword,[keyword2],･･･[keyword-n]};
  * @example		&seo(tag){tag,[tag2],･･･[tag-n]};
  * @license		Apache License 2.0
- * @version		0.1.1
+ * @version		0.2.0
+ * @since 		0.2.0 2020/10/18 ページキーワードの出力タグ変更、タグページ出力仕様変更
  * @since 		0.1.1 2019/11/04 Facebook及びTwitterのOGPタグ出力対応
  * @since 		0.1.0 2019/11/04 暫定初公開
  *
@@ -24,30 +25,40 @@ define('PLUGIN_SEO_FACEBOOK_OGP', 1);	// 1:有効 0:無効
 define('PLUGIN_SEO_TWITTER_OGP', 1);	// 1:有効 0:無効
 
 // タグページ名（階層構造のトップページ名）
-define('PLUGIN_SEO_TAG_PAGE', '');		// タグページを出力しない（初期値）
-//define('PLUGIN_SEO_TAG_PAGE', 'Tag');	// 「Tag」ページ名で階層構造でタグページを出力する
+define('PLUGIN_SEO_TAG_PAGE', 'Tag');	// 「Tag」ページ名で階層ページ出力
+define('PLUGIN_SEO_TAG_NAME', 'タグ');	// 「Tag」日本語名（キーワード出力）
 
 function plugin_seo_create_tag_page($tags) {
-										// タグページの存在確認
-	if (! is_page(PLUGIN_SEO_TAG_PAGE) ) {
-		// ページが存在しない場合はページを作成する
-		$postdata = '[[' . PLUGIN_SEO_TAG_PAGE . ']]' . "一覧\n\n" . '#related';
-		page_write(PLUGIN_SEO_TAG_PAGE, $postdata);
-	}
+	global $page_title;
+										// タグページ上書き
+	$contents  = '';
+	$contents .= "#setlinebreak(off)\n";
+	$contents .= "&seo(description){" . $page_title . PLUGIN_SEO_TAG_NAME
+		. "一覧};\n";
+	$contents .= "&seo(keywords){" . $page_title . ","
+		. PLUGIN_SEO_TAG_PAGE . "," . PLUGIN_SEO_TAG_NAME. "};\n";
+	$contents .= "#setlinebreak(on)\n\n";
+	$contents .= '[[' . PLUGIN_SEO_TAG_PAGE . ']]' . "一覧\n\n#related";
+	page_write(PLUGIN_SEO_TAG_PAGE, $contents);
 										// タグページ名セット
 	$pages = explode(",", $tags);
-										// 作成するタグページ内容
-	$postdata = '[[' . PLUGIN_SEO_TAG_PAGE . ']]' . "利用ページ一覧\n\n" . '#related';
 										// タグページ作成
 	foreach ($pages as $page) {
-		// タグページの階層構造としてその配下にページを作成する
+		// ページ内容編集
+		$contents  = '';
+		$contents .= "#setlinebreak(off)\n";
+		$contents .= "&seo(description){" . PLUGIN_SEO_TAG_NAME . '【'
+			. trim($page) . "】利用ページ一覧};\n";
+		$contents .= "&seo(keywords){" . $page_title . "," . trim($page) . ","
+			. PLUGIN_SEO_TAG_PAGE . "," . PLUGIN_SEO_TAG_NAME. "};\n";
+		$contents .= "#setlinebreak(on)\n\n";
+		$contents .= '[[' . PLUGIN_SEO_TAG_PAGE . ']]'
+			. "利用ページ一覧\n\n#related";
+		// タグページの階層構造としてその配下にページを作成（上書き）する
 		$page = PLUGIN_SEO_TAG_PAGE . '/' . trim($page);
-		if (! is_page($page) ) {
-			// ページが存在しない場合はページを作成する
-			page_write($page, $postdata);
-		}
+		page_write($page, $contents);
 	}
-	return '';
+	return;
 }
 
 function plugin_seo_inline() {
@@ -56,25 +67,37 @@ function plugin_seo_inline() {
 	$args = func_get_args();
 	$name = array_shift($args);
 	$content = array_pop($args);
+	$error = '';
 
 	// 引数による分岐
 	switch( $name ) {
 	case 'description':					// ページ概要
-	case 'keywords':					// ページキーワード
 		if ( isset($content) && ($content != '') ) {
-			// ページヘッダにページの概要タグ出力
-			$head_tags[] = '<meta name="' . $name . '" content="' . $content . '" />';
 										// Facebook OGPタグ出力
-			if ( (PLUGIN_SEO_FACEBOOK_OGP != 0) && ($name == 'description') ) {
+			if (PLUGIN_SEO_FACEBOOK_OGP != 0) {
 				// Facebook OGPタグ出力設定でページ概要の場合はタグを出力
-				$head_tags[] = '<meta property="og:' . $name . '" content="' . $content . '" />';
+				$head_tags[] = '<meta property="og:' . $name
+					. '" content="' . $content . '" />';
 			}
 										// Twitter OGPタグ出力
-			if ( (PLUGIN_SEO_TWITTER_OGP != 0) && ($name == 'description') ) {
+			if (PLUGIN_SEO_TWITTER_OGP != 0) {
 				// Twitter OGPタグ出力設定でページ概要の場合はタグを出力
-				$head_tags[] = '<meta property="twitter:' . $name . '" content="' . $content . '" />';
+				$head_tags[] = '<meta property="twitter:' . $name
+					. '" content="' . $content . '" />';
 			}
-			return '';
+		} else {
+			// エラーメッセージセット
+			$error = $name . 'の内容がありません';
+		}
+		break;
+	case 'keywords':					// ページキーワード
+		if ( isset($content) && ($content != '') ) {
+			// ページヘッダにページのキーワード（article:tag）タグ出力
+			$keywords = explode(",", $content);
+			foreach ($keywords as $value) {
+				$head_tags[] = '<meta property="article:tag" content="'
+					. $value . '" />';
+			}
 		} else {
 			// エラーメッセージセット
 			$error = $name . 'の内容がありません';
@@ -87,10 +110,6 @@ function plugin_seo_inline() {
 			if (PLUGIN_SEO_TAG_PAGE != '') {
 				// タグを出力するタグページ名の指定がある場合はページを出力する
 				plugin_seo_create_tag_page($content);
-				return '';
-			} else {
-				// タグ出力指定があっても、タグページ名の設定がない場合はスルー
-				return '';
 			}
 		} else {
 			// エラーメッセージセット
@@ -102,7 +121,8 @@ function plugin_seo_inline() {
 		$error = $name;
 	}
 
-	return '&seo 引数の指定にエラーがあります: ' . $error;
+	if ( isset($error) && ($error != '') ) {
+		return '&seo 引数の指定にエラーがあります: ' . $error;
+	}
 }
 
-?>
